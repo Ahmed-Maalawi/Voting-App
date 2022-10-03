8f32119249786931ca077835f317cbed7410971c
Ahmed El-Maalawi <ahmedelmaalawi@gmail.com>
2022-10-03 14:47:15 +0200

    Status Filters

---
+++ app/Http/Livewire/StatusFilters.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class StatusFilters extends Component
+{
+    public function render()
+    {
+        return view('livewire.status-filters');
+    }
+}
